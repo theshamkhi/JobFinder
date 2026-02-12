c5a24c40a56fabfa36e1b6a540771d1aaad8c9f2
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-02-12 14:46:11 +0100

    feat: add environment config with API URLs for The Muse and Arbeitnow

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
-
   production: false,
-  theMuseApiKey: 'f5bc564587075a77de3e313b2c485fdc0753b32d8cc1e65290d1a5be3b67526f'
+  jsonServerUrl: 'http://localhost:3000',
+  apis: {
+    themuse: {
+      baseUrl: 'https://www.themuse.com/api/public',
+      apiKey: 'f5bc564587075a77de3e313b2c485fdc0753b32d8cc1e65290d1a5be3b67526f'
+    },
+    arbeitnow: {
+      baseUrl: 'https://www.arbeitnow.com/api'
+    }
+  }
 };
