f1197a0d1efb77c897e6fe921f918f6015eeba2c
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-02-12 14:55:12 +0100

    feat: bootstrap app with NgRx store, effects, devtools, and HTTP interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
+import { favoritesReducer } from './store/favorites/favorites.reducer';
+import { FavoritesEffects } from './store/favorites/favorites.effects';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { isDevMode } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(
+      withInterceptors([errorInterceptor])
+    ),
+    provideStore({
+      favorites: favoritesReducer,
+    }),
+    provideEffects([FavoritesEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
 };
